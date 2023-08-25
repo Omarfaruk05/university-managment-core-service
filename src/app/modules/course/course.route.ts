1ab17e8ed56afd127c94e856153cf7c52d4f662a
unknown <mdomarfaruk149518@gmail.com>
2023-08-26 00:25:54 +0600

    [Done]create course

--- src/app/modules/course/course.route.ts
+++ src/app/modules/course/course.route.ts
@@ -1,13 +1,11 @@
 import express from 'express';
-import { ENUM_USER_ROLE } from '../../../enums/user';
-import auth from '../../middlewares/auth';
 import { CourseController } from './course.controller';
 
 const router = express.Router();
 
 router.post(
   '/',
-  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  //   auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
   CourseController.insertIntoDB
 );
 
--- src/app/modules/course/course.service.ts
+++ src/app/modules/course/course.service.ts
@@ -1,12 +1,59 @@
-import { Course } from '@prisma/client';
+import httpStatus from 'http-status';
+import ApiError from '../../../errors/ApiError';
 import prisma from '../../../shared/prisma';
+import { ICourseCreateData } from './course.interface';
 
-const insertIntoDB = async (data: Course): Promise<Course> => {
-  const result = await prisma.course.create({
-    data,
+const insertIntoDB = async (data: ICourseCreateData): Promise<any> => {
+  const { preRequisiteCourses, ...courseData } = data;
+
+  const newCourse = await prisma.$transaction(async transactionClient => {
+    const result = await transactionClient.course.create({
+      data: courseData,
+    });
+
+    if (!result) {
+      throw new ApiError(httpStatus.BAD_REQUEST, 'Unable to create course');
+    }
+
+    if (preRequisiteCourses && preRequisiteCourses.length > 0) {
+      for (let index = 0; index < preRequisiteCourses.length; index++) {
+        const createPrerequisite =
+          await transactionClient.courseToPrerequisite.create({
+            data: {
+              courseId: result.id,
+              preRequisiteId: preRequisiteCourses[index].courseId,
+            },
+          });
+        console.log(createPrerequisite);
+      }
+    }
+
+    return result;
   });
 
-  return result;
+  if (newCourse) {
+    const responsData = await prisma.course.findUnique({
+      where: {
+        id: newCourse.id,
+      },
+      include: {
+        preRequisite: {
+          include: {
+            preRequisite: true,
+          },
+        },
+        preRequisiteFor: {
+          include: {
+            course: true,
+          },
+        },
+      },
+    });
+
+    return responsData;
+  }
+
+  throw new ApiError(httpStatus.BAD_REQUEST, 'Unable to create course');
 };
 
 export const CourseService = {
