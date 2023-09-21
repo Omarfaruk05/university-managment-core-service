2de595c04095551ab4408149bb242df08c829c0f
unknown <mdomarfaruk149518@gmail.com>
2023-09-21 12:54:31 +0600

    [Done]Room

--- src/app/modules/room/room.route.ts
+++ src/app/modules/room/room.route.ts
@@ -1,14 +1,33 @@
 import express from 'express';
 import { ENUM_USER_ROLE } from '../../../enums/user';
 import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
 import { RoomController } from './room.controller';
+import { RoomValidation } from './room.validaton';
 
 const router = express.Router();
 
 router.post(
   '/',
   auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  validateRequest(RoomValidation.createRoomZodSchema),
   RoomController.insertIntoDB
+);
+
+router.get('/', RoomController.getAllFromDB);
+router.get('/:id', RoomController.getByIdFromDB);
+
+router.patch(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  validateRequest(RoomValidation.updateRoomZodSchema),
+  RoomController.updateOneInDB
+);
+
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  RoomController.deleteByIdFromDB
 );
 
 export const RoomRoutes = router;
